d5ea99d18f1829fb5a06210f1b42eff894aded56
rafaelpvs <rafael12wq@gmail.com>
2021-06-04 10:50:54 -0300

    Indica qual tela irá abrir ao clickar em login

--- app/src/main/java/com/example/aguaja/MainActivity.java
+++ app/src/main/java/com/example/aguaja/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.aguaja;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Button btnEmail = (Button) findViewById(R.id.btn_login);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnEmail.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_lista_prod);
+            }
+        });
+
     }
 }
