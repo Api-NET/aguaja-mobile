0872df29929e6e36dd7e84827b0ec9674da8ad60
Rafael Pinto <rafael12wq@gmail.com>
2021-06-07 08:48:06 -0300

    Cria tela de login

--- app/src/main/java/com/example/aguaja/MainActivity.java
+++ app/src/main/java/com/example/aguaja/MainActivity.java
@@ -2,25 +2,112 @@ package com.example.aguaja;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+
+public class MainActivity extends AppCompatActivity{
+    EditText email;
+    EditText senha;
+    TextView lblRest;
+    private String url = DomainName.NAME + "/api/client";
+
+
+    // Componentes do Volley
+    private RequestQueue meuQue;
+    private StringRequest minhaStringRequest;
+    private JsonArrayRequest jsonArrayRequest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Button btnEmail = (Button) findViewById(R.id.btn_login);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnEmail.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(View v) {
-                setContentView(R.layout.activity_lista_prod);
-            }
+
+    }
+    public void clica(View view) {
+         email = (EditText)findViewById(R.id.edit_email);
+         senha = (EditText)findViewById(R.id.edit_senha);
+        lblRest = (TextView)findViewById(R.id.text);
+        chamaLista();
+        login();
+    }
+    
+//    public void solicitaTodosclientes() {
+//        meuQue = Volley.newRequestQueue(this);
+//        minhaStringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+//            @Override
+//            public void onResponse(String response) {
+//                StringBuilder texto = new StringBuilder();
+//
+//                lblRest.setText(response);
+//            }
+//        }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                lblRest.setText("Erro na API" + error.toString());
+//                error.printStackTrace();
+//            }
+//        });
+//        meuQue.add(minhaStringRequest);
+//    }
+
+    public void login() {
+        meuQue = Volley.newRequestQueue(this);
+        jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        try {
+                            StringBuilder texto = new StringBuilder();
+                            for(int i = 0; i<response.length(); i++){
+                                JSONObject obj = response.getJSONObject(i);
+                                String email = obj.getString("email");
+                                String senha = obj.getString("password");
+                                checaCredenciais(email, senha);
+                            }
+                        }catch(JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    error.printStackTrace();
+                }
         });
+        meuQue.add(jsonArrayRequest);
+    }
+    public void checaCredenciais(String email, String senha) {
 
+        if (this.email.getText().toString().equals(email)  && this.senha.getText().toString().equals(senha)  ){
+            chamaLista();
+        }
+
+    }
+
+    private void chamaLista() {
+        Intent intent = new Intent(this, ListaProdActivity.class);
+        startActivity(intent);
     }
 }
