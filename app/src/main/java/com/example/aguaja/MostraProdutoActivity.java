a946c6c3be04417015b3d99a2af237f1ac68da8d
Rafael Pinto <rafael12wq@gmail.com>
2021-06-10 09:52:17 -0300

    Melhora tela de compra

--- app/src/main/java/com/example/aguaja/MostraProdutoActivity.java
+++ app/src/main/java/com/example/aguaja/MostraProdutoActivity.java
@@ -2,6 +2,7 @@ package com.example.aguaja;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,12 +10,14 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.aguaja.entity.Estoque;
 import com.example.aguaja.entity.Produto;
@@ -23,28 +26,35 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MostraProdutoActivity extends AppCompatActivity {
     private String url = DomainName.NAME + "/api/stock/";
+    private String urlPost = DomainName.NAME + "/api/order/";
     Button btnAumentaQtd;
     EditText quantidade;
     TextView valorPreco;
-    TextView textoProduto;
-
+    TextView nomeProduto;
+    TextView descricao;
+    TextView litrosProduto;
     Estoque est;
     private RequestQueue meuQue;
     private JsonObjectRequest jsonObjectRequest;
-
+    private StringRequest stringRequestPost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mostra_produto);
         btnAumentaQtd= (Button) findViewById(R.id.aumenta);
         quantidade = (EditText) findViewById(R.id.qtd);
+        descricao = (TextView) findViewById(R.id.descriçao);
         Bundle extras = getIntent().getExtras();
         valorPreco = findViewById(R.id.valorPreco);
-        textoProduto = findViewById(R.id.produto);
+        nomeProduto = findViewById(R.id.nome);
+        litrosProduto = findViewById(R.id.litros);
         meuQue = Volley.newRequestQueue(this);
         Integer idEst = Integer.parseInt( extras.getString("idEst"));
         Integer idProd = Integer.parseInt( extras.getString("idProd"));
@@ -76,8 +86,10 @@ public class MostraProdutoActivity extends AppCompatActivity {
                                     + "Litros: "+ produtos.get(idProd).getLitros() + "L\n"
                                     + "Descrição: \n"+ produtos.get(idProd).getDescricao();
                             System.out.println(texto);
-                            textoProduto.setText(texto);
-                            valorPreco.setText(""+custo);
+                            nomeProduto.setText(produtos.get(idProd).getNome());
+                            valorPreco.setText("Preço: R$"+custo);
+                            litrosProduto.setText(""+produtos.get(idProd).getLitros()+"L");
+                            descricao.setText(""+produtos.get(idProd).getDescricao());
                         }catch(JSONException e) {
                             e.printStackTrace();
                         }
@@ -99,7 +111,7 @@ public class MostraProdutoActivity extends AppCompatActivity {
         Integer qutd = Integer.parseInt(quantidade.getText().toString());
         if(qutd < est.getQuatidade()){
             quantidade.setText((qutd+1)+"");
-            valorPreco.setText((est.getPrecoVenda()*(qutd+1))+"");
+            valorPreco.setText("Preço: R$"+(est.getPrecoVenda()*(qutd+1))+"");
         }else {
             Toast toast = Toast.makeText(getApplicationContext(), "Quantidade máxima", Toast.LENGTH_LONG);
             toast.show();
@@ -110,8 +122,56 @@ public class MostraProdutoActivity extends AppCompatActivity {
         Integer qutd = Integer.parseInt(quantidade.getText().toString());
         if(qutd > 1){
             quantidade.setText((qutd-1)+"");
-            valorPreco.setText((est.getPrecoVenda()*(qutd-1))+"");
+            valorPreco.setText("Preço: R$"+(est.getPrecoVenda()*(qutd-1))+"");
         }
 
+    }
+
+    public void compra(View view) {
+        stringRequestPost = new StringRequest(Request.Method.POST, urlPost,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+                        System.out.println(response);
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                }
+        ) {
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("date", "2020-12-12");
+                params.put("discount", "50.2");
+                params.put("order_status", "1");
+                params.put("client_id", "1");
+                params.put("seller_id", "1");
+                params.put("Content-Type", "application/json; charset=utf-8");
+                params.put("Accept", "application/json; charset=utf-8");
+
+                return params;
+            }
+            @Override
+            public String getBodyContentType() {
+                return "application/json; charset=utf-8";
+            }
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("Content-Type", "application/json; charset=utf-8");
+                return params;
+            }
+
+
+
+        };
+        meuQue.add(stringRequestPost);
     }
 }
